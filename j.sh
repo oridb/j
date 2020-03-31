a3a67c5200da8b1b5e5f63276c76125907942e71
Ori Bernstein <ori@eigenstate.org>
2020-03-31 23:11:21 +0000

    Initial versions of the script.

---
+++ j.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$EDITOR" ]; then
+	editor=vi
+else
+	editor=$EDITOR
+fi
+journal=$HOME/journal
+tmpfile=/tmp/j.$$
+trap "rm -f $tmpfile" EXIT
+touch $tmpfile
+today=$(date -I)
+lastdate=$(grep '^[0-9]' $journal | tail -n 1)
+if [ -z "$*" ]; then
+	echo $*
+	$editor $tmpfile
+else
+	echo $* >$tmpfile
+fi
+(
+	if [ "$today" = "$lastdate" ]; then
+		echo
+	else
+		echo $today
+	fi
+	sed 's/^/   /g' <$tmpfile
+)>>$journal
