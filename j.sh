7c6f38ab84c6b977698b4d6277c257580654de78
Sam Lazarus <lazarus.sam@gmail.com>
2020-04-03 20:42:27 -0400

    makes rating a proper command line argument with getopt

--- j.sh
+++ j.sh
@@ -23,9 +23,24 @@ today=$(date -I)
 now=$(date -Iseconds)
 lastdate=$(grep '^[0-9]' $journal | tail -n 1)
 
-# regex for matching rational entry ratings in the range [0, 7)
-# these have to be the first argument in the command line or the first line in the entry
-ratingregex="^[0-6]\.[0-9]*$"
+# handle command line arguments
+args=`getopt r: $*`
+if [ $? -ne 0 ]; then
+        echo 'usage: j [-r rating] [msg...]'
+        exit 2
+fi
+set -- $args
+
+# get variables for arguments
+rating=""
+while [ $# -ne 0 ]
+do
+	case "$1" in
+		-r) rating="$2"; shift;;
+		--) shift; break;;
+	esac
+	shift
+done
 
 # handle the entry
 if [ -z "$*" ]; then
@@ -34,18 +49,18 @@ if [ -z "$*" ]; then
 	$editor $tmpfile
 else
 	# text was specified as arguments
-	# if the first argument is a rating, prepend it to the temp file
-	if [ $# -gt 1 -a $(expr "$1" : ^[0-7]$) -gt 0 -o $(expr "$1" : $ratingregex) -gt 0 ]
-	then
-		echo $1 >>$tmpfile
-		shift
-	fi
-	# echo remaining arguments into the temp file
 	echo $* >>$tmpfile
 fi
 
-# prepend the timestamp -- if we did this before, it would be visible in the editor
-echo "$now\n$(cat $tmpfile)" > $tmpfile
+# indent the body of the entry by two spaces
+sed -i 's/^/  /g' $tmpfile
+
+# prepend the timestamp and metadata-- if we had done this before, it would be visible in the editor
+metadata=""
+if [ ! -z $rating ]; then
+	metadata=" rating: $rating"
+fi
+echo "[$now]$metadata\n$(cat $tmpfile)" > $tmpfile
 
 # append the entry to the actual journal file
 (
