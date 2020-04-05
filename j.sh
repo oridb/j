7aefbeac0ef0e8740828e561cd9c60304ec39f53
samson212 <lazarus.sam@gmail.com>
2020-04-04 23:45:47 -0400

    Timestamps & Ratings (#1)
    - prepends timestamp to each entry
    - adds a newline after the date and increases indentation to four spaces
    - add a newline before each day
    - makes rating a proper command line argument with getopt
    - switches to "HHhMM -XXXX" timestamp format
    - Sync header formatting with rc version

--- j.sh
+++ j.sh
@@ -2,28 +2,75 @@
 
 set -x
 
+# default editor to vi
 if [ -z "$EDITOR" ]; then
 	editor=vi
 else
+# otherwise use the system editor
 	editor=$EDITOR
 fi
+
+# file references
 journal=$HOME/journal
 tmpfile=/tmp/j.$$
+
+# file handling
 trap "rm -f $tmpfile" EXIT
 touch $tmpfile
+
+# date variables
 today=$(date -I)
-lastdate=$(grep '^[0-9]' $journal | tail -n 1)
+now=$(date +"%Hh%M %z")
+lastdate=$(grep '^===' $journal | tail -n 1 | sed 's/ *=== *//g')
+
+# handle command line arguments
+args=`getopt r: $*`
+if [ $? -ne 0 ]; then
+        echo 'usage: j [-r rating] [msg...]'
+        exit 2
+fi
+set -- $args
+
+# get variables for arguments
+rating=""
+while [ $# -ne 0 ]
+do
+	case "$1" in
+		-r) rating="$2"; shift;;
+		--) shift; break;;
+	esac
+	shift
+done
+
+# handle the entry
 if [ -z "$*" ]; then
+	# no arguments, go to the editor
 	echo $*
 	$editor $tmpfile
 else
-	echo $* >$tmpfile
+	# text was specified as arguments
+	echo $* >>$tmpfile
 fi
+
+# indent the body of the entry by two spaces
+sed -i 's/^/  /g' $tmpfile
+
+# prepend the timestamp and metadata-- if we had done this before, it would be visible in the editor
+metadata=""
+if [ ! -z $rating ]; then
+	metadata=" rating: $rating"
+fi
+echo "[$now]$metadata\n$(cat $tmpfile)" > $tmpfile
+
+# append the entry to the actual journal file
 (
-	if [ "$today" = "$lastdate" ]; then
+	# prepend the date and a newline if it's the first entry for the day
+	if ! [ "$today" = "$lastdate" ]; then
 		echo
-	else
-		echo $today
+		echo "=== $today ==="
 	fi
-	sed 's/^/   /g' <$tmpfile
+	echo "  "
+
+	# now indent another two spaces and output the entry and heading
+	sed 's/^/  /g' <$tmpfile
 )>>$journal
